37ef1918c6fa23ea511f91bded9a9122b027e35a
anshthakur0999 <80532812+anshthakur0999@users.noreply.github.com>
2025-03-18 14:03:35 +0000

    variable,first program

---
+++ Variables and datatypes/main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+
+    int age = 21;
+    cout<<"My age is : "<<age<<endl;
+
+    // int age;
+    // cout<<age<<endl;
+
+    int count = 5;
+    float pi = 3.14;
+    char alphabet = 'A';
+    double weight = 55.6987;
+    bool isMale = 1;
+    bool isChild = true;
+
+    cout<<count<<endl;
+    cout<<pi<<endl;
+    cout<<alphabet<<endl;
+    cout<<weight<<endl;
+    cout<<isMale<<endl;
+    cout<<isChild<<endl;
+
+
+
+    return 0;
+}
